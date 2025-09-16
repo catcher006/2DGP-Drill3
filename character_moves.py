8a6eb7e8622c01c981cf39918107aa345f8b9515
catcher006 <seungho217@tukorea.ac.kr>
2025-09-16 11:10:13 +0900

    캐릭터 원 운동 함수 완성

--- character_moves.py
+++ character_moves.py
@@ -11,16 +11,22 @@ def move_rectangle():
 
 def move_circle():
     print("Moving circle")
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(0.1)
+
+    r = 200
+
+    for deg in range(0, 360, 10):
+        x = 400 + r * math.cos(math.radians(deg))
+        y = 300 + r * math.sin(math.radians(deg))
+
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.1)
     pass
 
 
 while True:
     move_circle()
     move_rectangle()
-    #break
+    # break
     pass
-
 close_canvas()
